6fe941a624f1c4e4a9bd4d20b880f1cbd7f3914a
parmanand99 <56157826+parmanand99@users.noreply.github.com>
2022-06-14 10:25:48 +0530

    just updated

---
+++ 10.html-dom/app.js
@@ -0,0 +1,6 @@
+document.getElementById("header").innerHTML="<h1>Welcome to HTML DOM</h1>"
+document.getElementById("header").style.backgroundColor="Yellow"
+
+function callMe(){
+    alert('did you call me ?')
+}
